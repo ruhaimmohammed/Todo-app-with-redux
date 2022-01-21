bbf12b5449d0fdf8f8b368633b8dfe0fd4755794
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2022-01-22 04:35:47 +0530

    Basic designs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Input from './components/Input';
+
+function App() {
+  return (
+    <div className="App">
+      {/* todo list */}
+      {/* done button */}
+      <Input />
+    </div>
+  );
+}
+
+export default App;
